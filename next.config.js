e6acf9e5d58a7caba5fb3fb0a5712e291e79ea97
Vincentius Roger Kuswara <vincentiusrkuswara@gmail.com>
2022-08-12 17:14:40 +0800

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
+  /* config options here */
   reactStrictMode: true,
-  swcMinify: true,
-}
 
-module.exports = nextConfig
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // eslint-disable-next-line no-param-reassign
+      config.resolve.fallback = {
+        fs: false,
+        child_process: false,
+      };
+    }
+
+    return config;
+  },
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
+};
+
+module.exports = nextConfig;
